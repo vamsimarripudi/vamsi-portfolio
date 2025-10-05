2d7f8b4a7230beef1b34b367cb2a9ca6eb309772
vamsi marripudi <vmarripudi49@gmail.com>
2025-10-05 22:56:22 +0530

    upadting app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 
 
 
-const App = () => (
+/*const App = () => (
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<Home />} />
@@ -13,6 +13,14 @@ const App = () => (
     <Route path="/contact" element={<Contact />} />
   </Routes>
   </BrowserRouter>
-)
+)*/
+const App = () => (
+<div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: '100vh', textAlign: 'center' }}>
+  <img style={{height:"400px"}} src="https://media.istockphoto.com/id/1495430612/vector/coming-soon-with-colorful-cut-out-foil-ribbon-confetti-background.jpg?s=612x612&w=0&k=20&c=0HjZUHqlNnUedZmJCbMpooiY92f4tgGCh_jZK51zSmU=" alt="Vamsi Krishna" />
+<p>Stay tuned for more updates!</p>
+  
+</div>
+  )
+
 
 export default App
